419abcc9a3b4ed51c243f7636a5cafc22455a4d9
timas <vas.liwanov@yandex.ru>
2020-11-19 21:28:37 +0300

    added burger handler

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,106 @@
 $(function () {
-    const headerBtnNextImg = $('.header__btn_right');
-    const headerBtnPrevImg = $('.header__btn_left');
+    const btnBurger1 = $('#burger1');
+    const btnTopElem = $('#burger1 .burger__elem_top');
+    const btnMiddleElem = $('#burger1 .burger__elem_middle');
+    const btnBottomElem = $('#burger1 .burger__elem_bottom');
+    const overlay = $('.overlay');
 
-    headerBtnNextImg.on('click', () => {
+    btnBurger1.on('click', function () {
+        if ($(this).hasClass("burger_closed")) {
+            btnTopElem.animate()
+            btnMiddleElem.animate({
+                opacity: 0
+            }, {
+                duration: 100
+            });
+            btnBottomElem.animate()
 
-        console.log('next');
+            $(this)
+                .removeClass("burger_closed")
+                .addClass("burger_opened");
+
+            return;
+        }
+
+        if ($(this).hasClass("burger_opened")) {
+            btnTopElem.animate()
+            btnMiddleElem.animate({
+                opacity: 1
+            });
+            btnBottomElem.animate()
+
+            $(this)
+                .removeClass("burger_opened")
+                .addClass("burger_closed");
+
+            return;
+        }
     })
 
-    headerBtnPrevImg.on('click', () => {
-        console.log('prev');
+    const btnBurger2 = $('#burger2');
+    const btnTopElem2 = $('#burger2 .burger__elem_top');
+    const btnMiddleElem2 = $('#burger2 .burger__elem_middle');
+    const btnBottomElem2 = $('#burger2 .burger__elem_bottom');
+
+    btnBurger2.on('click', function () {
+        if ($(this).hasClass("burger_closed")) {
+            btnTopElem2.animate()
+            btnMiddleElem2.animate({
+                opacity: 0
+            }, {
+                duration: 100
+            });
+            btnBottomElem2.animate()
+
+            $(this)
+                .removeClass("burger_closed")
+                .addClass("burger_opened");
+
+            return;
+        }
+
+        if ($(this).hasClass("burger_opened")) {
+            btnTopElem2.animate()
+            btnMiddleElem2.animate({
+                opacity: 1
+            });
+            btnBottomElem2.animate()
+
+            $(this)
+                .removeClass("burger_opened")
+                .addClass("burger_closed");
+
+            return;
+        }
     })
+
+    new Glide('.glide', {
+        type: 'carousel',
+        // autoplay: 3000
+    }).mount()
 })
 
-new Glide('.glide', {
-    type: 'carousel',
-    autoplay: 3000
-}).mount()
+
+function closeMobileMenu(menuBtnObj, headerContainer, headerNav) {
+    if (menuBtnObj.menu.hasClass("mobile-menu_opened")) {
+        menuBtnObj.topElem.animate()
+        menuBtnObj.middleElem.animate({
+            opacity: 1
+        });
+        menuBtnObj.bottomElem.animate()
+
+        headerContainer
+            .removeClass("header__container_moved")
+            .addClass("header__container")
+
+        headerNav
+            .removeClass("navigate_opened")
+            .addClass("navigate_closed")
+
+
+        menuBtnObj.menu
+            .removeClass("mobile-menu_opened")
+            .addClass("mobile-menu_closed");
+        return;
+    }
+}
