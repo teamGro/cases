420b9c7f0754811684285e058d8fddbc61f9f921
timas <vas.liwanov@yandex.ru>
2020-11-21 14:34:44 +0300

    fixes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,130 +1,146 @@
-$(function () {
-    let sliderElem;
+let sliderElem;
 
-    const btnBurger1 = $('#burger1');
-    const btnTopElem = $('#burger1 .burger__elem_top');
-    const btnMiddleElem = $('#burger1 .burger__elem_middle');
-    const btnBottomElem = $('#burger1 .burger__elem_bottom');
-    const mobileMenu = $('.header__mobile-menu');
+const btnBurger1 = $('#burger1');
+const btnTopElem = $('#burger1 .burger__elem_top');
+const btnMiddleElem = $('#burger1 .burger__elem_middle');
+const btnBottomElem = $('#burger1 .burger__elem_bottom');
+const mobileMenu = $('.header__mobile-menu');
 
-    btnBurger1.on('click', function () {
-        if ($(this).hasClass("burger_closed")) {
-            btnTopElem.animate()
-            btnMiddleElem.animate({
-                opacity: 0
-            }, {
-                duration: 100
-            });
-            btnBottomElem.animate();
+btnBurger1.on('click', function () {
+    if ($(this).hasClass("burger_closed")) {
+        btnTopElem.animate()
+        btnMiddleElem.animate({
+            opacity: 0
+        }, {
+            duration: 100
+        });
+        btnBottomElem.animate();
 
-            mobileMenu.addClass('header__mobile-menu_open');
-            sliderElem.pause();
+        mobileMenu.addClass('header__mobile-menu_open');
+        sliderElem.pause();
 
-            $(this).
-                animate()
-                .removeClass("burger_closed")
-                .addClass("burger_opened");
+        $(this).
+            animate()
+            .removeClass("burger_closed")
+            .addClass("burger_opened");
 
-            return;
-        }
+        return;
+    }
 
-        if ($(this).hasClass("burger_opened")) {
-            btnTopElem.animate()
-            btnMiddleElem.animate({
-                opacity: 1
-            });
-            btnBottomElem.animate();
+    if ($(this).hasClass("burger_opened")) {
+        btnTopElem.animate()
+        btnMiddleElem.animate({
+            opacity: 1
+        });
+        btnBottomElem.animate();
 
-            mobileMenu.removeClass('header__mobile-menu_open');
-            sliderElem.play();
+        mobileMenu.removeClass('header__mobile-menu_open');
+        sliderElem.play();
 
-            $(this).
-                animate()
-                .removeClass("burger_opened")
-                .addClass("burger_closed");
+        $(this).
+            animate()
+            .removeClass("burger_opened")
+            .addClass("burger_closed");
 
-            return;
-        }
-    })
-
-    const btnBurger2 = $('#burger2');
-    const btnTopElem2 = $('#burger2 .burger__elem_top');
-    const btnMiddleElem2 = $('#burger2 .burger__elem_middle');
-    const btnBottomElem2 = $('#burger2 .burger__elem_bottom');
-    const mobileMenu2 = $('.header__mobile-menu_sec');
-
-    btnBurger2.on('click', function () {
-        if ($(this).hasClass("burger_closed")) {
-            btnTopElem2.animate()
-            btnMiddleElem2.animate({
-                opacity: 0
-            }, {
-                duration: 100
-            });
-            btnBottomElem2.animate();
-
-            mobileMenu2.addClass('header__mobile-menu_open');
-            sliderElem.pause();
-
-            $(this)
-                .removeClass("burger_closed")
-                .addClass("burger_opened");
-
-            return;
-        }
-
-        if ($(this).hasClass("burger_opened")) {
-            btnTopElem2.animate()
-            btnMiddleElem2.animate({
-                opacity: 1
-            });
-            btnBottomElem2.animate();
-
-            mobileMenu2.removeClass('header__mobile-menu_open');
-            sliderElem.play();
-
-            $(this)
-                .removeClass("burger_opened")
-                .addClass("burger_closed");
-
-            return;
-        }
-    })
-
-    sliderElem = new Glide('.glide', {
-        type: 'carousel',
-        autoplay: 3000
-    })
-    sliderElem.mount();
-
-    let mixer = mixitup('.goods__list');
-
-    const filtersBtnsContainer = $('.filters');
-    currentActiveFilter = $('.filters__item_active');
-    filtersBtnsContainer.on('click', (e) => {
-        var target = $(e.target);
-
-        if (target.prop('tagName') == 'BUTTON') {
-            target = target.parent();
-        }
-
-        currentActiveFilter.removeClass('filters__item_active');
-        target.addClass('filters__item_active');
-        currentActiveFilter = target;
-    });
-
-    const navBar = $('.nav');
-    navBar.on("click", (e) => {
-        let target = $(e.target);
-        if (target.prop("tagName") == "A") e.preventDefault();
-        let scrollName = target.attr("data-scroll");
-        let scrollElem = $(`#${scrollName}`);
-        let scrollTop = scrollElem.offset().top;
-
-        $('html, body').animate({
-            scrollTop: scrollTop
-        }, 500);
-    })
+        return;
+    }
 })
+
+const btnBurger2 = $('#burger2');
+const btnTopElem2 = $('#burger2 .burger__elem_top');
+const btnMiddleElem2 = $('#burger2 .burger__elem_middle');
+const btnBottomElem2 = $('#burger2 .burger__elem_bottom');
+const mobileMenu2 = $('.header__mobile-menu_sec');
+
+btnBurger2.on('click', function () {
+    if ($(this).hasClass("burger_closed")) {
+        btnTopElem2.animate()
+        btnMiddleElem2.animate({
+            opacity: 0
+        }, {
+            duration: 100
+        });
+        btnBottomElem2.animate();
+
+        mobileMenu2.addClass('header__mobile-menu_open');
+        sliderElem.pause();
+
+        $(this)
+            .removeClass("burger_closed")
+            .addClass("burger_opened");
+
+        return;
+    }
+
+    if ($(this).hasClass("burger_opened")) {
+        btnTopElem2.animate()
+        btnMiddleElem2.animate({
+            opacity: 1
+        });
+        btnBottomElem2.animate();
+
+        mobileMenu2.removeClass('header__mobile-menu_open');
+        sliderElem.play();
+
+        $(this)
+            .removeClass("burger_opened")
+            .addClass("burger_closed");
+
+        return;
+    }
+})
+
+sliderElem = new Glide('.glide', {
+    type: 'carousel',
+    autoplay: 3000
+})
+sliderElem.mount();
+
+
+
+let mixer = mixitup('.goods__list');
+
+let goodsSlider = new Glide('.glide2', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1
+});
+goodsSlider.mount();
+
+const filtersBtnsContainer = $('.filters');
+currentActiveFilter = $('.filters__item_active');
+filtersBtnsContainer.on('click', (e) => {
+    var target = $(e.target);
+
+    if (target.prop('tagName') == 'BUTTON') {
+        target = target.parent();
+    }
+
+    currentActiveFilter.removeClass('filters__item_active');
+    target.addClass('filters__item_active');
+    currentActiveFilter = target;
+
+    goodsSlider.update({
+        //perView: 3
+    });
+});
+
+const navBar = $('.nav');
+navBar.on("click", (e) => {
+    let target = $(e.target);
+    if (target.prop("tagName") == "A") e.preventDefault();
+    let scrollName = target.attr("data-scroll");
+    let scrollElem = $(`#${scrollName}`);
+    let scrollTop = scrollElem.offset().top;
+
+    $('html, body').animate({
+        scrollTop: scrollTop
+    }, 500);
+
+});
+
+
+
+
 
 
